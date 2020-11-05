d982e2e1df307309f45fd4a229072dd44e840259
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-11-04 22:25:35 -0300

    refactor: isolando variáveis relacionadas ao bdd

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,13 @@
 import path from 'path'
+import { HOST_MYSQL, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD } from './src/config/database-config'
 
 module.exports = {
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1',
-    database: 'fifo-server',
-    user: 'root',
-    password: ''
+    host: HOST_MYSQL,
+    database: DATABASE_NAME,
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
