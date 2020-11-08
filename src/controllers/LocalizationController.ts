b550533d1ed0350b577bcdb363ba4f95c70e27c5
Leandro  Bergamo <leandro.bergamo@hotmail.com>
2020-11-07 22:02:26 -0300

    docs: localization controller

--- src/controllers/LocalizationController.ts
+++ src/controllers/LocalizationController.ts
@@ -4,6 +4,12 @@ import Helpers from '@helpers/index'
 import db from '@database/connection'
 
 class LocalizationController {
+  /**
+   * Realiza a criação
+   * @param req Request
+   * @param res Response
+   * @return Response
+   */
   async save (req: Request, res: Response): Promise<Response> {
     try {
       Helpers.existsOrError(req.body.name, 'Nome da localização não informado')
@@ -18,6 +24,12 @@ class LocalizationController {
     return res.status(201).send(objectData)
   }
 
+  /**
+  * Realiza a pesquisa
+  * @param req Request
+  * @param res Response
+  * @return Response
+  */
   async get (req: Request, res: Response): Promise<Response> {
     const localization = new Localization()
     const findLocalization = await localization.get()
@@ -28,6 +40,12 @@ class LocalizationController {
     }
   }
 
+  /**
+  * Realiza a pesquisa pelo ID
+  * @param req Request - request ID
+  * @param res Response
+  * @return Response
+  */
   async find (req: Request, res: Response): Promise<Response> {
     const localization = new Localization()
     const findLocalization = await localization.findId(req.params.id)
@@ -38,7 +56,14 @@ class LocalizationController {
     }
   }
 
+  /**
+  * Realiza o Update pelo id
+  * @param req Request - required ID
+  * @param res Response
+  * @return Response
+  */
   async update (req: Request, res: Response): Promise<Response> {
+    if (req.body.id == null) return res.status(401).send({ message: 'Falta o id' })
     const localization = new Localization()
     localization.make(req.body)
     const objectData = await localization.save()
@@ -48,6 +73,12 @@ class LocalizationController {
     return res.status(201).json(objectData)
   }
 
+  /**
+  * Realiza a exclusão pelo ID
+  * @param req Request - required ID
+  * @param res Response
+  * @return Response
+  */
   async delete (req: Request, res: Response): Promise<Response> {
     if (req.body.id == null) return res.status(401).send({ message: 'Falta o id' })
     const localization = new Localization()
