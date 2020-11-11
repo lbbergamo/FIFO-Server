3b74e521a15e680583416b17a79f263576d25075
Leandro Bergamo <leandro.bergamo@hotmail.com>
2020-11-11 13:09:34 -0300

    Fix/cors (#20)
    * feat: user
    
    * feat: user
    
    * fix: delete_migration_users
    
    * fix: gitignore
    
    Co-authored-by: Sergio Silva <sergio.bezerra@outlook.com>

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,38 +2,36 @@
 import Database from './Database'
 
 class User extends Database {
-    protected db = {
-        Entity: 'user',
-        RequiredFields: [ 'id', 'name', 'email', 'last_socket_id', 'localization_id', 'cover' ],
-        Secure: [ 'id' ]
-    }
+  protected db = {
+    Entity: 'user',
+    RequiredFields: ['id', 'name', 'email', 'last_socket_id', 'localization_id', 'cover'],
+    Secure: ['id']
+  }
 
-    public make (object: any) {
-        this.data = new UserModel(object)
-    }
+  public make (object: any) {
+    this.data = new UserModel(object)
+  }
 }
 
 class UserModel implements IData {
-    readonly id: number
-    name: string
-    email: string
-    cover: string
-    last_socket_id: number
-    localization_id: number    
+  readonly id: number
+  name: string
+  email: string
+  cover: string
+  last_socket_id: number
+  localization_id: number
 
-    constructor (object: any) {
-        this.id = object.id
-        this.name = object.name
-        this.email = object.email
-        this.cover = object.cover
-        this.last_socket_id = object.last_socket_id
-        this.localization_id = object.localization_id
-        
-    }
+  constructor (object: any) {
+    this.name = object.name
+    this.email = object.email
+    this.cover = object.cover
+    this.last_socket_id = object.last_socket_id
+    this.localization_id = object.localization_id
+  }
 
-    get UserModel () {
-        return this
-    }
+  get UserModel () {
+    return this
+  }
 }
 
 export default User
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import routes from './routes/index'
+import cors from 'cors'
 
 const app = express()
 export const port = process.env.PORT || 3333
-
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
