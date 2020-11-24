d1351a02cad4bd370d154be52364a4ab3839ad7a
Leandro  Bergamo <leandro.bergamo@hotmail.com>
2020-11-24 16:53:45 -0300

    feat: setup inicial

---
+++ src/core/config-swagger.ts
@@ -0,0 +1,7 @@
+import { serve, setup } from 'swagger-ui-express'
+import { Express } from 'express'
+import swaggerConfig from '@docs/index'
+
+export default (app: Express): void => {
+  app.use('/api-docs/', serve, setup(swaggerConfig))
+}
