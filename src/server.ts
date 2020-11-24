4bbd74ec83e4e3fb03821874061bb61d87641230
Leandro  Bergamo <leandro.bergamo@hotmail.com>
2020-11-23 21:36:01 -0300

    feat: alterando o arquivo para o core

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,5 @@
-import express from 'express'
-import routes from './routes/index'
-import cors from 'cors'
-import { Server, Socket } from 'socket.io'
-import http from 'http'
+import app from '@core/app'
 
-require('dotenv/config')
-
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server, {
-  cors: {
-    origin: '*'
-  }
+app.server.listen(process.env.PORT, function () {
+  console.log(`Server sendo iniciado com a porta:" ${process.env.PORT}`)
 })
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-
-io.on('connection', (socket: Socket) => {
-  socket.on('cadadastro_fila', (msg) => {
-    console.log(msg)
-  })
-  socket.emit('status', '4546')
-})
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500)
-  res.json({ error: error.message })
-})
-
-server.listen(process.env.PORT, () => console.log(`\n\n\n **** Server is running, port: ${process.env.PORT} ****\n\n\n`))
