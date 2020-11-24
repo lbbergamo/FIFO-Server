f5761a01494dcf64a794dc98239e657ad520b398
Leandro  Bergamo <leandro.bergamo@hotmail.com>
2020-11-24 17:00:26 -0300

    docs: iniciando

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,3 +1,8 @@
 export default {
-
+  openapi: '3.0.0',
+  info: {
+    title: 'FIFO - Server - API',
+    description: 'Api para o desafio do processo de seleção da FCAMARA',
+    version: '1.0.0'
+  }
 }
