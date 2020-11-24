03ec8c0088af35c61c45be9e98207bb967f0192d
Leandro  Bergamo <leandro.bergamo@hotmail.com>
2020-11-24 16:08:30 -0300

    test: getGroup

--- src/models/listQueue.spec.ts
+++ src/models/listQueue.spec.ts
@@ -89,4 +89,27 @@ describe('ListQueue', () => {
     object.remove({ socketId: 'socket id4', service: 4, localization: 121, user: 12 })
     expect(object.getList().length).toBe(3)
   })
+  it('getGroup item', () => {
+    const object = makeSut()
+    const data = new Date()
+    object.add({ socketId: 'aqui é o socket id 1 ', service: 2, localization: 1, user: 121, date: data })
+    object.add({ socketId: 'aqui é o socket id 2 ', service: 2, localization: 1, user: 121, date: data })
+    object.add({ socketId: 'aqui é o socket id 3 ', service: 2, localization: 1, user: 121, date: data })
+    object.add({ socketId: 'socket id2', service: 4, localization: 2, user: 121, date: data })
+    object.add({ socketId: 'socket id2', service: 3, localization: 2, user: 121, date: data })
+    object.add({ socketId: 'socket id3', service: 1, localization: 2, user: 121, date: data })
+    object.add({ socketId: 'socket id4', service: 4, localization: 2, user: 121, date: data })
+    const list = object.getGroup()
+    const teste = []
+    for (const localization in list) {
+      for (const service in list[localization]) {
+        // let i = 0
+        list[localization][service].forEach((value) => {
+          teste.push(value)
+          // i++
+        })
+      }
+    }
+    expect(object.getList().length).toBe(teste.length)
+  })
 })
