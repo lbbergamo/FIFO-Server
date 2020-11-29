08f4b4d418ab753be70ae2ef01f1cb884d7b442d
Leandro Bergamo <leandro.bergamo@hotmail.com>
2020-11-29 19:59:25 -0300

    fix: remocao do cover

--- src/models/admin/Localization.ts
+++ src/models/admin/Localization.ts
@@ -3,7 +3,7 @@ import Database from './../Database'
 class Localization extends Database {
   protected db = {
     Entity: 'localization',
-    RequiredFields: ['id', 'name', 'cover', 'notes', 'description'],
+    RequiredFields: ['id', 'name', 'notes', 'description'],
     Secure: ['name', 'description']
   }
 
@@ -15,14 +15,12 @@ class Localization extends Database {
 class LocalizationModel implements IData {
   readonly id: number
   name: string
-  cover: string
   notes: string
   description: string
 
   constructor (object: any) {
     this.id = object.id
     this.name = object.name
-    this.cover = object.cover
     this.notes = object.notes
     this.description = object.description
   }
